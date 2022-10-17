9b5400096716c9baac6d5d5e4a7aa9ab9425c6e8
Ricardo <ricardo.carreira@advancedsecure.co.uk>
2022-10-17 10:51:30 +0100

    :( :(

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ import { Octokit } from "octokit";
 try {
     // `who-to-greet` input defined in action metadata file
     const token = getInput('package-token');
-    const version = getInput('package-version');
+    //const version = getInput('package-version');
     const packageType = "nuget"
-
 
     const octokit = new Octokit({
         auth: `${token}`
@@ -23,14 +22,14 @@ try {
 
     setOutput("names", JSON.stringify(packagesNames, undefined, 2));
 
-    packagesNames.forEach(async name => {
+    //packagesNames.forEach(async name => {
 
-        const packageVersion = await octokit.request('GET /user/packages/{package_type}/{package_name}/versions', {
-            package_type: `${packageType}`,
-            package_name: `${name}`
-        })
-        console.log(`The event payload: ${JSON.stringify(packageVersion, undefined, 2)}`);
-    });
+    //    const packageVersion = await octokit.request('GET /user/packages/{package_type}/{package_name}/versions', {
+    //        package_type: `${packageType}`,
+    //        package_name: `${name}`
+    //    })
+    //    console.log(`The event payload: ${JSON.stringify(packageVersion, undefined, 2)}`);
+    //});
 
 } catch (error) {
 
